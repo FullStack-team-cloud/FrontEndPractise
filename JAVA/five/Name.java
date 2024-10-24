16c7b994ca0d3fa0e8af480be32cbbc9ad3d9f59
PAVAN KUMAR <pavankumar26122003@gmail.com>
2024-10-24 16:11:40 +0530

    I added java program

---
+++ JAVA/five/Name.java
@@ -0,0 +1,35 @@
+// Naming conventions for class names and method names
+public class Name {
+    public static void main(String args[]) {
+        String text= """
+        In Java, the naming conventions for class names and method names are as follows:
+
+        Class Name Naming Conventions:
+
+        1. Class names should start with a capital letter.
+        2. Use nouns or noun phrases.
+        3. Avoid using abbreviations unless widely accepted (e.g., HTML, API).
+        4. Use camel case (e.g., MyClass, PrintWriter).
+        5. Avoid using reserved words (e.g., class, interface).
+
+        Examples:
+
+        public class Employee {}
+        public class HelloWorld {}
+
+
+        Method Name Naming Conventions:
+
+        1. Method names should start with a lowercase letter.
+        2. Use verbs or verb phrases.
+        3. Describe the method's action.
+        4. Use camel case (e.g., calculateSalary, printMessage).
+        5. Avoid using reserved words.
+
+        Examples:
+
+        public void calculateSalary() {}
+        public void printMessage() {} """;
+        System.out.println(text);
+    }
+}
