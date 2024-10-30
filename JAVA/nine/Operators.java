09f38f84b07c8437756d921f8211331cf1b78ddb
FullStack-team-cloud <lathateam2024gmail.com>
2024-10-30 06:31:38 +0530

    added Operators

---
+++ JAVA/nine/Operators.java
@@ -0,0 +1,15 @@
+public class Operators{
+  public static void main(String args[]){
+    int x=999999,y=111111;
+    int sum=x+y;
+    int sub=x-y;
+    int mul=x*y;
+    int div=x/y;
+    int mod=x%y;
+    System.out.println("Addition is:"+sum);
+    System.out.println("Subtraction is:"+sub);
+    System.out.println("Multiplication is:"+mul);
+    System.out.println("Division is:"+div);
+    System.out.println("Module is:"+mod);
+  }
+}
