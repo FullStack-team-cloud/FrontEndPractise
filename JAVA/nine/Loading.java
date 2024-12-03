7553953805aa85a954a59b6ba1ba94dd5157c645
FullStack-team-cloud <lathateam2024gmail.com>
2024-12-03 05:57:43 +0530

    this is Methodoverloading

---
+++ JAVA/nine/Loading.java
@@ -0,0 +1,16 @@
+public class Loading {
+  public static void main (String[] args){
+  System.out.println(Loading.methodOverLoading());
+  System.out.println(Loading.methodOverLoading1());
+  System.out.println(Loading.methodOverLoading2());
+  }
+  public static String methodOverLoading(){
+    return "reddy";
+  }
+  public static String methodOverLoading1(){
+    return "final";
+  }
+  public static boolean methodOverLoading2(){
+    return true;
+  }
+}
